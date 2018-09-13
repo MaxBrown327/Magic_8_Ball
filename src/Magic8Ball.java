ee6b2e819b4df671c0de243e8dc7a7a34abfdf5d
Max <Maximillian.Brown.2021@student.lhschools.org>
2018-09-13 13:03:27 -0400

    Added Intellij and started project

---
+++ src/Magic8Ball.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+
+    public static void main(String[]args) {
+        //Code goes here\\
+        Scanner keyboard;
+
+
+
+
+
+
+
+
+    }
+
+}
