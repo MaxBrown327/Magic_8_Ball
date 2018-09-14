a1a60610134d377de618303a92bc0e8b2e73e420
Max <Maximillian.Brown.2021@student.lhschools.org>
2018-09-14 13:36:25 -0400

    Minor Updates

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -4,13 +4,26 @@ public class Magic8Ball {
 
     public static void main(String[]args) {
         //Code goes here\\
-        Scanner keyboard;
+        Scanner keyboard = new Scanner(System.in);
+        int testing;
+        String question;
 
+        //Initialization phase\\
+        System.out.println("What is your question?");
+        question = keyboard.nextLine();
+        System.out.println("Now pick a number 1-100");
+        testing = keyboard.nextInt();
+        if (testing<10) {
+            System.out.println("Not very good");
 
+        }
 
-
-
-
+        if (testing>10) if (testing<21){
+            System.out.println("That's pretty good");
+        }
+        if (testing>20) if (testing<31){
+            System.out.println("Not too likely");
+        }
 
 
     }
